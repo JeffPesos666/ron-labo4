8c2b063bd7639d0cda15b4287f9b704ee7f492f9
Louis-Xavier Michaud <1326113@cgodin.qc.ca>
2025-09-15 13:39:04 -0400

    page 13 manque juste le binding

--- app/src/main/java/ca/qc/cgodin/labo4recyclerview/MainActivity.kt
+++ app/src/main/java/ca/qc/cgodin/labo4recyclerview/MainActivity.kt
@@ -1,15 +1,23 @@
 package ca.qc.cgodin.labo4recyclerview
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import ca.qc.cgodin.labo4recyclerview.adapter.ItemAdapter
 import ca.qc.cgodin.labo4recyclerview.data.DataSource
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var layoutManager: RecyclerView.LayoutManager
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -24,5 +32,32 @@ class MainActivity : AppCompatActivity() {
         val recyclerView = findViewById<RecyclerView>(R.id.rvAffirmations)
         recyclerView.adapter = ItemAdapter(this, myDataset)
 
+        setSupportActionBar(binding.toolbar)
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.linearItem -> {
+                layoutManager = LinearLayoutManager(this@MainActivity)
+                binding.rvAffirmations.layoutManager = layoutManager
+                true
+            }
+            R.id.gridItem -> {
+                layoutManager = GridLayoutManager(this@MainActivity, 2)
+                binding.rvAffirmations.layoutManager = layoutManager
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
     }
 }
