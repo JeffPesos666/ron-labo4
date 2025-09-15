27ee7d1d14c59cb526f3a94e4d72999bb8d94680
JeffPesos666 <1326113@cgodin.qc.ca>
2025-09-15 19:59:37 -0400

    peak

--- app/src/main/java/ca/qc/cgodin/labo4recyclerview/MainActivity.kt
+++ app/src/main/java/ca/qc/cgodin/labo4recyclerview/MainActivity.kt
@@ -12,25 +12,34 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import ca.qc.cgodin.labo4recyclerview.adapter.ItemAdapter
 import ca.qc.cgodin.labo4recyclerview.data.DataSource
+import ca.qc.cgodin.labo4recyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var layoutManager: RecyclerView.LayoutManager
 
+    private lateinit var  binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
         val myDataset = DataSource().loadAffirmations()
-        val recyclerView = findViewById<RecyclerView>(R.id.rvAffirmations)
-        recyclerView.adapter = ItemAdapter(this, myDataset)
+        binding.rvAffirmations.adapter = ItemAdapter(this, myDataset)
+
 
         setSupportActionBar(binding.toolbar)
 
